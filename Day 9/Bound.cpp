8f089a3c7a1ba93552f9aa8f6a247f677a75bbab
Gaurav Parkhe <gauravparkhe2027gmail.com>
2026-07-31 23:36:16 +0530

    Day 9 moved

---
+++ Day 9/Bound.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+int firstOcc(int arr[],int n,int key)
+{
+    int s =0,e=n-1;
+    int mid=s+(e-s)/2;
+    int ans =-1;
+
+    while (s<=e)    
+    {
+        if(arr[mid]==key)
+        {
+            ans= mid;
+            e=mid-1;
+        }
+        else if(key>arr[mid])
+        {
+            s=mid+1;
+        }
+        else if(key<arr[mid])
+        {
+            e=mid-1;
+        }
+
+        mid = s+(e-s)/2;
+    }
+    return ans;
+}
+
+int lastOcc(int arr[],int n,int key)
+{
+    int s =0,e=n-1;
+    int mid=s+(e-s)/2;
+    int ans =-1;
+
+
+    while (s<=e)    
+    {
+        if(arr[mid]==key)
+        {
+            ans= mid;
+            s=mid+1;
+        }
+        else if(key>arr[mid])
+        {
+            s=mid+1;
+        }
+        else if(key<arr[mid])
+        {
+            e=mid-1;
+        }
+
+        mid = s+(e-s)/2;
+    }
+    return ans;
+}
+
+ int main()
+ {
+    int arr[12]={1,2,3,3,3,3,3,3,3,3,3,4};
+    cout<<"the first Occurance of 3 is at position :"<<firstOcc(arr,12,3)<<endl;
+    cout<<"the first Occurance of 3 is at position :"<<lastOcc(arr,12,3)<<endl;
+
+ }

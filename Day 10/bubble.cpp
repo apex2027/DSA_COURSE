a3a485d5cebd3acf19da695cf938922ca0611533
Gaurav Parkhe <gauravparkhe2027gmail.com>
2026-08-02 23:03:45 +0530

    day 10 added successfully

--- Day 10/bubble.cpp
+++ Day 10/bubble.cpp
@@ -6,7 +6,7 @@ void bubble(int arr[],int n)
 {
     for(int i = 0;i<n-1;i++)
     {
-        int minIndex= i;
+        
         for(int j = 0;j<n-1;j++)
         {
             if(arr[j] >arr[j+1])
